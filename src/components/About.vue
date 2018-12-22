08a84bfaecf48b5f8be1dc5403891ed132ebd972
Jonsonice <415410795@qq.com>
2018-12-22 15:57:47 +0800

    脚手架搭建以及导航路由搭建

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="about">
+		<h1>hello about</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'about',
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Customers.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="customers">
+		<h1>hello </h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'customers',
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
